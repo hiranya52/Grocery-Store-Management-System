58d6d34d8ba710908bf495ce00fae6817c0e180a
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-09-28 23:42:25 +0530

    Implement DBConnection singleton class

--- GroceryStoreManagementSystem/src/com/grocerystore/management/db/DBConnection.java
+++ GroceryStoreManagementSystem/src/com/grocerystore/management/db/DBConnection.java
@@ -4,10 +4,30 @@
  */
 package com.grocerystore.management.db;
 
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author hiranyamendis
  */
 public class DBConnection {
+    
+    private java.sql.Connection connection;  
+   private static DBConnection dBConnection;
+   private DBConnection() throws ClassNotFoundException, SQLException{
+       Class.forName("com.mysql.cj.jdbc.Driver");
+       connection=DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "12345678");
+   }
+   public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+       if(dBConnection==null){
+           dBConnection=new DBConnection();
+       }
+       return dBConnection;
+   }
+   public java.sql.Connection getConnection(){
+       return connection;
+   }
     
 }
